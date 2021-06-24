a8ff8dc374a8845f6f1c41bc1ef2f3b7407f3640
root <enmuhammet23@gmail.com>
2021-06-24 22:49:00 +0300

    Final Modifications

--- main.cpp
+++ main.cpp
@@ -1,32 +1,29 @@
-#include <iostream>
 #include <fstream>
 #include <vector>
 #include <QtCore>
 #include <QApplication>
 #include <QMainWindow>
-#include "myTable.h"
 #include <QVBoxLayout>
+#include "myTable.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     ifstream inFile;
-    inFile.open("givens.txt");
-    vector<QString> givens;
-    string line = "";
+    inFile.open(getenv("MYCRYPTOCONVERT")); // Get the filename from environment variables
+    vector<QString> givens; // Names and/or symbols given
+    string line = ""; // Current line to read
     while (getline(inFile, line))
-    {
-        givens.push_back(QString::fromStdString(line));
-    }
-    QApplication a(argc, argv);
-    QMainWindow window;
-    QWidget *centralWidget = new QWidget;
+        givens.push_back(QString::fromStdString(line)); // Read the lines 
+    QApplication a(argc, argv); // Initialize Qt application
+    QMainWindow window; // Create the main window to be able to resize the table
+    QWidget *centralWidget = new QWidget; 
     window.setCentralWidget(centralWidget);
     QGridLayout *centralLayout = new QGridLayout(centralWidget);
     MyTable *m = new MyTable(givens);
     centralLayout->addWidget(m);
-    window.resize(620, 400);
-    window.show();
+    window.resize(620, 400); // Initial size of the window
+    window.show(); 
     return a.exec();
 }
--- myTable.cpp
+++ myTable.cpp
@@ -5,80 +5,74 @@
 #include <QSizePolicy>
 MyTable::MyTable(vector<QString> wantedCurrencies) : QTableWidget()
 {
-    this->wantedCurrencies = wantedCurrencies;
-    this->table = new QTableWidget(this);
-    this->table->setRowCount(wantedCurrencies.size());
-    this->table->setColumnCount(3);
-    table->setMinimumSize(600, 420);
-    QStringList verticalLabels = QStringList::fromVector(QVector<QString>::fromStdVector(wantedCurrencies));
+    this->wantedCurrencies = wantedCurrencies; 
+    this->table = new QTableWidget(this); // Create a table widget
+    this->table->setRowCount(wantedCurrencies.size()); // Initiate the number of rows
+    this->table->setColumnCount(3); // 3 Columns: USD, EUR, GBP
+    table->setMinimumSize(600, 420); // Size of the table
+    QStringList verticalLabels = QStringList::fromVector(QVector<QString>::fromStdVector(wantedCurrencies)); //from QString vector to QStringList
+    this->table->setVerticalHeaderLabels(verticalLabels);
+
     this->topLabels.push_back(QString::fromStdString("USD"));
     this->topLabels.push_back(QString::fromStdString("EUR"));
     this->topLabels.push_back(QString::fromStdString("GBP"));
-
     QStringList horizontalLabels = QStringList::fromVector(QVector<QString>::fromStdVector(topLabels));
-    this->table->setVerticalHeaderLabels(verticalLabels);
     this->table->setHorizontalHeaderLabels(horizontalLabels);
-    // this->table->horizontalHeader()->sectionResizeMode()
 
-    manager = new QNetworkAccessManager(this);
-    connect(manager, SIGNAL(finished(QNetworkReply *)), this, SLOT(nameToId(QNetworkReply *)));
-    manager->get(QNetworkRequest(QUrl(QString::fromStdString("https://api.coingecko.com/api/v3/coins/list"))));
+    manager = new QNetworkAccessManager(this); // Initialize the network manager to make requests
+    connect(manager, SIGNAL(finished(QNetworkReply *)), this, SLOT(nameToId(QNetworkReply *))); // Run nameToId function after the response came.
+    manager->get(QNetworkRequest(QUrl(QString::fromStdString("https://api.coingecko.com/api/v3/coins/list")))); // Make the request to list endpoint
 }
 
 void MyTable::nameToId(QNetworkReply *coinList)
 {
-    disconnect(manager, SIGNAL(finished(QNetworkReply *)), this, SLOT(nameToId(QNetworkReply *)));
-
-    QString data = coinList->readAll();
-    QJsonDocument jsonResponse = QJsonDocument::fromJson(data.toUtf8());
-    QJsonArray jsonArray = jsonResponse.array();
+    disconnect(manager, SIGNAL(finished(QNetworkReply *)), this, SLOT(nameToId(QNetworkReply *)));  // We connected nameToId function to manager but we need to
+                                                                                                    // run another function so disconnect and connect another one.
+    QString data = coinList->readAll(); // Read the reply
+    QJsonDocument jsonResponse = QJsonDocument::fromJson(data.toUtf8()); // reply to json
+    QJsonArray jsonArray = jsonResponse.array(); // Endpoint returns us a list so we need to use QJsonArray 
 
     for (const QString currency : this->wantedCurrencies)
     {
         for (const auto obj : jsonArray)
-        {
+        {   // If the name or symbol of a coın is equal to wanted coin, add to vector
             if (obj.toObject()["name"].toString().toLower() == currency.toLower() || obj.toObject()["symbol"].toString().toLower() == currency.toLower())
             {
                 coinIDs.push_back(obj.toObject().value("id").toString());
-                qDebug() << currency << obj.toObject() ;
-                continue;
+                break;
             }
         }
     }
     // qDebug() << coinIDs;
 
     string url = "https://api.coingecko.com/api/v3/simple/price?ids=";
-    for (int i = 0; i < coinIDs.size() - 1; i++)
+    for (int i = 0; i < int(coinIDs.size() - 1); i++)
     {
-        url += coinIDs[i].toLower().toStdString() + ",";
+        url += coinIDs[i].toLower().toStdString() + ","; // Fence Problem, add '{name} ,' until the last one
     }
-    url += coinIDs[coinIDs.size() - 1].toStdString() + "&vs_currencies=usd,eur,gbp";
-    qDebug() << "URL:" << QString::fromStdString(url);
-    connect(manager, SIGNAL(finished(QNetworkReply *)), this, SLOT(insertData(QNetworkReply *)));
-    manager->get(QNetworkRequest(QUrl(QString::fromStdString(url))));
+    url += coinIDs[coinIDs.size() - 1].toStdString() + "&vs_currencies=usd,eur,gbp"; // Just add '{name}', without comma
+    connect(manager, SIGNAL(finished(QNetworkReply *)), this, SLOT(insertData(QNetworkReply *))); // 
+    manager->get(QNetworkRequest(QUrl(QString::fromStdString(url)))); // Make request to get the prices
 }
 void MyTable::insertData(QNetworkReply *reply)
 {
-    QString data = reply->readAll();
-    QJsonDocument jsonResponse = QJsonDocument::fromJson(data.toUtf8());
+    QString data = reply->readAll(); // Read the reply
+    QJsonDocument jsonResponse = QJsonDocument::fromJson(data.toUtf8()); // Encode the data
     QJsonObject jsonObject = jsonResponse.object();
-    for (int i = 0; i < this->coinIDs.size(); i++)
+    for (int i = 0; i < int(this->coinIDs.size()); i++) // Iterate over the coins
     {
         
-        QString currency = this->coinIDs[i];
-        QJsonObject x = jsonObject[currency].toObject();
-        qDebug() << currency << x;
-        for (int j = 0; j < this->topLabels.size(); j++)
+        QString currency = this->coinIDs[i]; //Get coin id
+        QJsonObject x = jsonObject[currency].toObject(); //Get the JsonObject by giving id
+        // qDebug() << currency << x;
+        for (int j = 0; j < int(this->topLabels.size()); j++) // Toplabels are usd, eur, and gbp
         {
-            QString label = this->topLabels[j];
+            QString label = this->topLabels[j]; 
             QJsonValueRef y = x[label.toLower()];
-            QTableWidgetItem *item = new QTableWidgetItem();
-            item->setData(Qt::DisplayRole, y.toDouble());
-            this->table->setItem(i, j, item);
-            // this->table->setItem(i, j, new QTableWidgetItem(tr("%1").arg(y.toDouble())));
+            QTableWidgetItem *item = new QTableWidgetItem();// Create new table item
+            item->setData(Qt::DisplayRole, y.toDouble()); // Insert the price to the item
+            this->table->setItem(i, j, item); // Insert the item to table
         }
     }
-    this->table->resizeColumnsToContents();
-
-    this->table->update();
+    this->table->resizeColumnsToContents(); // Resize the column widths to make the prices visible
 }
